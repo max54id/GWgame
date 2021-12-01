b0b0f7fb1cdb74219a3696a0af24f506339a7e13
max54id <30610851+max54id@users.noreply.github.com>
2021-12-01 16:06:41 +0800

    1201

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -781,10 +781,7 @@ var UnityLoader = UnityLoader || {
     if (!UnityLoader.SystemInfo.hasWebGL) {
       unityInstance.popup("Your browser does not support WebGL",
         [{text: "OK", callback: onerror}]);
-    }
-
-
-
+    } 
      else if (["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) < -1) {
       unityInstance.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.",
         [{text: "OK", callback: onsuccess}]);
